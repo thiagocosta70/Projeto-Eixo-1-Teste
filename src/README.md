4a58b2deb76fdf683f09fb6b82088ac795b2cf78
thiagocosta70 <thiagoemanuel2504@gmail.com>
2024-05-20 18:36:23 -0300

    update

---
+++ src/README.md
@@ -0,0 +1,114 @@
+# Instruções de utilização
+
+## Estratégia de Organização de Codificação
+
+Nesse primeiro eixo do curso, para simplificar a utilização do Git e a organização das pastas e artefatos de implementação no sistema de arquivos, sugerimos que o projeto seja estruturado de modo que cada aluno trabalhe com seus arquivos nas suas respectivas pastas, identificadas por nomes das suas respectivas telas. Por exemplo:
+
+- Pasta src (root do projeto): index.html, index.css, index.js - Desenvolvedora responsável: Aluna X.
+- Pasta src\clientes: clientes.html, clientes.css, clientes.js - Desenvolvedor responsável: Aluno Y.
+- Pasta src\funcionarios: funcionarios.html, funcionarios.css, funcionarios.js - Desenvolvedora responsável: Aluna Z.
+
+## Instalação do Site
+
+O site em HTML/CSS/JS é um projeto estático. É necessário implantá-lo em um servidor web de sua preferência. Existem diversos servidores web gratuitos que podem ser utilizados, tal como GitHub Pages (GitHub.IO), Vercel, Render, Netlify, Surge.sh, entre outros. Nesse primeiro eixo, recomenda-se inclusive a utilização do GitHub Pages. [Insira o endereço eletrônico público para acessá-lo.]
+
+## Histórico de versões
+
+### [0.1.0] - 01/05/2024
+
+#### Adicionado
+
+- Página home e sua estilização
+- Arquivo javascript para funcionalidade do Carousel contido na home
+
+#### Alterado
+
+- Ajustes nas documentações 01, 02 e 04
+
+### [0.1.1] - 03/05/2024
+
+#### Alterado
+
+- Correção de ajustes na página home
+
+### [0.1.2] - 03/05/2024
+
+#### Adicionado
+
+- Página de cadastro e sua estilização
+
+### [0.1.3] - 04/05/2024
+
+#### Adicionado
+
+- Página de cardápio e sua estilização
+- Arquivo javascript para funcionalidade do Carousel dos itens do cardápio
+
+### [0.1.4] - 05/05/2024
+
+#### Adicionado
+
+- Página de pedidos.
+- Arquivo javascript para funcionalidade de avaliação e tempo do pedido.
+
+### [0.1.5] - 08/05/2024
+
+#### Alterado
+
+- Correção na Página de pedidos e ajustes no arquivo javascript.
+
+### [0.1.6] - 09/05/2024
+
+#### Adicionado
+
+- Adicionada funcionalidades javascript da página de cadastro
+
+#### Alterado
+
+- HTML e CSS da página de cadastro.
+- Link para cadastro na navbar da página home
+
+### [0.1.7] - 09/05/2024
+
+#### Adicionado
+
+- Página Restaurates e suas estilização.
+
+### [0.1.8] - 10/05/2024
+
+#### Adicionado
+
+- Página do Login e sua estilização.
+- Arquivo Javascript para funcionalidades na página de Login.
+
+### [0.1.9] - 10/05/2024
+
+#### Adicionado
+
+- Página de carrinho e sua estilização.
+- Arquivo Javascript para funcionalidades do carrinho.
+
+### [0.1.10] - 11/05/2024
+
+#### Alterado
+
+- Padronização da página de pedidos
+- Ajustes no JavaScript de avaliação e contagem regressiva
+
+### [0.1.11] - 11/05/2024
+
+#### Alterado
+
+- Ajustes dos links nos arquivos HTML
+
+### [0.1.12] - 12/05/2024
+
+#### Alterado
+
+- Padronização do nome dos arquivos para incial minúscula
+
+### [0.1.13] - 14/05/2024
+
+#### Alterado
+
+- Alteração para a página de cardápio renderizar via JSON
---
+++ src/assets/js/cardapioCarouselScript.js
@@ -0,0 +1,106 @@
+import { restaurantes } from './restauranteJson.js';
+
+// Obtém o ID do restaurante da URL e renderiza o menu se o restaurante for encontrado.
+const id = getRestaurantId();
+if (id) {
+    const restauranteObj = restaurantes.find(restaurante => restaurante.idRestaurante == id);
+    if (restauranteObj) {
+        const categoriesItems = getCategoriesArray(restauranteObj)
+        renderMenu(restauranteObj);
+        renderCardapio(categoriesItems, restauranteObj)
+        document.querySelector('.restaurante-nome').textContent = restauranteObj.nomeRestaurante;
+    } else {
+        window.location.href = "../pages/restaurantes.html"
+    }
+} else {
+    window.location.href = "../pages/restaurantes.html"
+}
+
+// seção que gerencia as categorias e o carrossel
+
+const produtos = document.querySelectorAll(".filtros-cardapio>li>button");
+
+produtos.forEach(element => {
+    element.addEventListener("click", handleButtonClick);
+});
+
+window.addEventListener('load', function() {
+    const activePill = this.document.querySelector(".filtros-cardapio .active");
+    const pillName = activePill.getAttribute('aria-controls');
+    const pillCards = document.getElementById(pillName);
+    const cardapioSection = pillCards.querySelector('.cardapio-items');
+    const carouselItem = pillCards.querySelectorAll('.carousel-item');
+    const cardQuantity = carouselItem.length;
+    if (cardQuantity < 4) {
+        updateCardSize(cardQuantity, carouselItem, cardapioSection);
+    }
+});
+
+function handleButtonClick() {
+    const pillName = this.getAttribute('aria-controls');
+    const pillCards = document.getElementById(pillName);
+    const cardapioSection = pillCards.querySelector('.cardapio-items');
+    const carouselItem = pillCards.querySelectorAll('.carousel-item');
+    const cardQuantity = carouselItem.length;
+    if (cardQuantity < 4) {
+        updateCardSize(cardQuantity, carouselItem, cardapioSection);
+    }
+}
+
+
+function updateCardSize(cardQuantity, carouselItem, cardapioSection) {
+    carouselItem.forEach(element => {
+        element.style.flex = `0 0 calc(100%/${cardQuantity})`
+        if (window.matchMedia("(min-width:576px)").matches) {
+            let percentage = 0;
+            switch (cardQuantity) {
+                case 1:
+                    percentage = '25%'
+                    break;
+                case 2:
+                    percentage = '35%'
+                    break;
+                case 3:
+                    percentage = '55%'
+                    break;
+                }
+            cardapioSection.style.maxWidth = percentage;
+        }
+    });
+
+}
+
+const carousels = document.querySelectorAll('.carousel-inner');
+const multipleItemCarousel = document.querySelectorAll('.carousel');
+
+carousels.forEach(function(carousel) {
+    if (window.matchMedia("(min-width:576px)").matches) {
+        var cardWidth = carousel.querySelector('.carousel-item').offsetWidth;
+
+        var scrollPosition = 0
+
+        carousel.parentElement.querySelector('.carousel-control-next').addEventListener('click', function(){
+            var carouselWidth = carousel.scrollWidth;
+            var cardWidth = carousel.querySelector('.carousel-item').offsetWidth;
+            if (scrollPosition < (carouselWidth - (cardWidth * 5))) {
+                scrollPosition = scrollPosition + cardWidth;
+                carousel.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+            }
+        });
+
+        carousel.parentElement.querySelector('.carousel-control-prev').addEventListener('click', function(){
+            var cardWidth = carousel.querySelector('.carousel-item').offsetWidth;
+            if (scrollPosition > 0) {
+                scrollPosition = scrollPosition - cardWidth;
+                carousel.scrollTo({ left: scrollPosition, behavior: 'smooth' });
+            }
+        });
+
+        scrollPosition = scrollPosition + cardWidth;
+    } 
+    else {
+        multipleItemCarousel.forEach(element => {
+            element.parentElement.classList.add('slide');
+        });
+    }
+});
